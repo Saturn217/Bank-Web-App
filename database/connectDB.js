09189607dcc23475b8d771d05cf9544a572233d4
0xSaturn <mubarakoladele98@gmail.com>
2026-04-07 15:42:44 +0100

    Refactor: remove commented-out code and clean up connectDB function

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,36 +1,8 @@
-// const mongoose = require("mongoose")
-
-// let connectionPromise = null
-
-// const connectDB= async()=>{
-//     if(mongoose.connection.readyState ===1) return
-
-//     if (connectionPromise) return connectionPromise
-
-//     connectionPromise = mongoose
-//     .connect(process.env.DATABASE_URI)
-//     .then(()=>{
-//         console.log("Database connected successfully");
-        
-//     })
-//     .catch((err)=>{
-//         connectionPromise=null;
-//         console.log(err);
-//         throw err;
-        
-//     })
-
-//     return connectionPromise
-// }
-
-// module.exports= connectDB
 
 
 
-// //0 ->disconnected
-// //1 ->connected
-// //2->connecting
-// //3 ->disconnecting
+
+
 
 const mongoose = require("mongoose");
 
